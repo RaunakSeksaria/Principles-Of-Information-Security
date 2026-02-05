0f0f4f220f981a979dd6f2a56d6aec9b732ccec8
RaunakSeksaria <seksariaraunak@gmail.com>
2026-02-06 03:10:34 +0530

    math-utils-2-will-remove

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 pub mod utils;
+#[cfg(test)]
+mod tests;
+// pub mod primitives;
+
 // pub mod primitives;
--- src/utils/math_utils.rs
+++ src/utils/math_utils.rs
@@ -1,13 +1,6 @@
 //Some of the utilities here are already implemented in external crates; but doing them here for learning purposes
 use num_bigint::BigUint;
 use num_integer::Integer;
-// use super::*;
-
-// pub fn mod_exponentiation(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
-//     // Implement modular exponentiation (base^exponent mod modulus)
-//     // Placeholder implementation
-//     BigUint::from(0u32)
-// }
 
 pub fn mod_exponentiation_u32(base: u32, exponent: u32, modulus: u32) -> u32 {
     assert!(modulus!=0,"Modulus cannot be zero");
@@ -24,7 +17,7 @@ pub fn mod_exponentiation_u32(base: u32, exponent: u32, modulus: u32) -> u32 {
         exp_bin>>=1;
         base_curr = ((base_curr as u64 * base_curr as u64) % modulus as u64) as u32;
     }
-    return result;
+    result
 }
 
 pub fn mod_exponentiation_num_bigint(base: BigUint, exponent: BigUint, modulus: BigUint)-> BigUint{
@@ -43,25 +36,5 @@ pub fn mod_exponentiation_num_bigint(base: BigUint, exponent: BigUint, modulus: 
         exp_bin >>=1u32;
         base_curr = (&base_curr * &base_curr)% &modulus;
     }
-    return result;
+    result
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_mod_exp_u32(){
-        let result = mod_exponentiation_u32(5,13,23);
-        assert_eq!(result,21);
-            // 2^10 mod 1000 = 24
-        assert_eq!(mod_exponentiation_u32(2, 10, 1000), 24);
-        
-        // 3^0 mod 7 = 1
-        assert_eq!(mod_exponentiation_u32(3, 0, 7), 1);
-
-    }
-    
-}
-// pub fn main(){
-//     print!("{}\n",mod_exponentiation_u32(5,13,23)) // gives wrong answer
-// }
